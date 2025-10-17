a078bb32daf47021408e5b694c4069771928a09e
likhithabai-g <likhithabai08@gmail.com>
2025-10-17 11:27:54 +0530

    third commit TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
 import ProgressTracker from "./Components/ProgressTracker";
+import { useEffect, useState } from "react";
+import "./Style.css";
 
 export default function App() {
+  const  [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  });
+  
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
+
   return(
     <div>
       <h1>Planify</h1>
       <p>Plan it.Do it.Done</p>
 
-      <Taskform />
-      <TaskList />
-      <ProgressTracker />
+      <Taskform addTask = {addTask}/>
+      <TaskList  tasks = {tasks}
+      updateTask={updateTask}
+      deleteTask={deleteTask}/>
+      <ProgressTracker task = {tasks}/>
 
       <button>Clear all tasks</button>
     </div>
